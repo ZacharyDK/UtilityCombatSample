1908c5e261eaf94549bc878dded168f89b1a1fe0
Zach Kolansky <zacharykolansky@gmail.com>
2022-05-05 14:17:01 -0500

    I love typos

--- Plugins/UtilityCombatPlugin/Source/UtilityCombatPlugin/Public/WeaponFiringComponent.h
+++ Plugins/UtilityCombatPlugin/Source/UtilityCombatPlugin/Public/WeaponFiringComponent.h
@@ -276,7 +276,7 @@ public:
 	UFUNCTION(Server,Reliable)
 	void FireLineTraceServer(const FVector StartLocation, const FRotator InitialRotation) const;
 
-	UFUNCTION(BlueprintCallabl, Category = Basic)
+	UFUNCTION(BlueprintCallable, Category = Basic)
 	void FirePattern();
 
 
